9ef5f1bb807dcef5a4a90abf518e46545e3e5e07
vishal_g@tekdi.net <vishal_g@tekdi.net>
2015-07-18 17:10:34 +0530

    created new api for handle user follow process

--- easysocial/easysocial/follower.php
+++ easysocial/easysocial/follower.php
@@ -24,7 +24,153 @@ class EasysocialApiResourceFollower extends ApiResource
 
 	public function post()
 	{
-		$this->plugin->setResponse('Post method not allowed for this api, use another method.');
+		$this->plugin->setResponse($this->follow());
+	}
+	
+	/*
+	 *Function for follow user 
+	 */
+	public function follow()
+	{
+		$app = JFactory::getApplication();
+		$target_user = $app->input->get('target_user',0,'INT');
+		$type = $app->input->get('type','user','STRING');
+		$group = $app->input->get('group','user','STRING');
+		$log_user 	= $this->plugin->get('user')->id;
+		$follow 	= $app->input->get('follow',1,'INT');
+		
+		$target 	= FD::user( $target_user );
+		
+		$res = new stdClass();
+		if( $target_user == $log_user )
+		{
+			$res->success = 0;
+			$res->message = "You can not follow yourself";
+		}
+		
+		// Load subscription table.
+		$subscription 	= FD::table('Subscription');
+
+		// Get subscription library
+		$subscriptionLib 	= FD::get('Subscriptions');
+		// Determine if the current user is already a follower
+		$isFollowing 	= $subscriptionLib->isFollowing( $target_user , $type , $group , $log_user );
+		
+		$subscription->uid 		= $target_user;
+		$subscription->type 	= $type . '.' . $group;
+		$subscription->user_id	= $log_user;
+		$points = FD::points();
+
+		if(!$isFollowing)
+		{
+			//$state 	= $subscription->store();
+			if($subscription->store())
+			{
+				$state = $this->addbadges( $target,$log_user,$subscription->id );
+		
+				$res->success = 1;
+				$res->message = "Following ".$target->username;
+				
+				return $res;
+			}
+			else
+			{
+				$res->success = 0;
+				$res->message = "unable to follow ".$target->username;
+				return $res;
+			}
+			
+		}
+		else
+		{
+			if( $subscriptionLib->unfollow( $target_user, $type, $group, $log_user ) )
+			{
+				// @points: profile.unfollow
+				// Assign points when user starts new conversation
+				
+				$points->assign( 'profile.unfollow' , 'com_easysocial' , $log_user);
+
+				// @points: profile.unfollowed
+				// Assign points when user starts new conversation
+				$points->assign( 'profile.unfollowed' , 'com_easysocial' , $user->id );
+				$res->success = 1;
+				$res->message = "successfully unfollow ".$target->username;
+				
+				return $res;
+			}
+			else
+			{
+				$res->success = 0;
+				$res->message = "unable to unfollow ".$target->username;
+				
+				return $res;
+			}
+		}
+		
+	}
+	
+	/*
+	 *Function for add badges 
+	 */
+	public function addbadges( $user,$log_user,$sub_id )
+	{
+		$my 	= FD::user( $log_user );
+		// @badge: followers.follow
+		$badge 	= FD::badges();
+		$badge->log( 'com_easysocial' , 'followers.follow' , $my->id , JText::_( 'COM_EASYSOCIAL_FOLLOWERS_BADGE_FOLLOWING_USER' ) );
+
+		// @badge: followers.followed
+		$badge->log( 'com_easysocial' , 'followers.followed' , $user->id , JText::_( 'COM_EASYSOCIAL_FOLLOWERS_BADGE_FOLLOWED' ) );
+
+		// @points: profile.follow
+		// Assign points when user follows another person
+		$points = FD::points();
+		$points->assign( 'profile.follow' , 'com_easysocial' , $my->id );
+
+		// @points: profile.followed
+		// Assign points when user is being followed by another person
+		$points->assign( 'profile.followed' , 'com_easysocial' , $user->id );
+
+		// check if admin want to add stream on following a user or not.
+		$config = FD::config();
+		if ($config->get( 'users.stream.following')) {
+			// Share this on the stream.
+			$stream 			= FD::stream();
+			$streamTemplate		= $stream->getTemplate();
+
+			// Set the actor.
+			$streamTemplate->setActor( $my->id , SOCIAL_TYPE_USER );
+
+			// Set the context.
+			$streamTemplate->setContext( $sub_id , SOCIAL_TYPE_FOLLOWERS );
+
+			// Set the verb.
+			$streamTemplate->setVerb( 'follow' );
+
+			$streamTemplate->setAccess( 'followers.view' );
+
+			// Create the stream data.
+			$stream->add( $streamTemplate );
+		}
+
+        // Set the email options
+        $emailOptions   = array(
+            'title'     	=> 'COM_EASYSOCIAL_EMAILS_NEW_FOLLOWER_SUBJECT',
+            'template'		=> 'site/followers/new.followers',
+            'actor'     	=> $my->getName(),
+            'actorAvatar'   => $my->getAvatar(SOCIAL_AVATAR_SQUARE),
+            'actorLink'     => $my->getPermalink(true, true),
+            'target'		=> $user->getName(),
+            'targetLink'	=> $user->getPermalink(true, true),
+            'totalFriends'		=> $my->getTotalFriends(),
+            'totalFollowing'	=> $my->getTotalFollowing(),
+            'totalFollowers'	=> $my->getTotalFollowers()
+        );
+
+
+		$state 	= FD::notify('profile.followed' , array($user->id), $emailOptions, array( 'url' => $my->getPermalink(false, false, false) ,  'actor_id' => $my->id , 'uid' => $user->id ));
+		
+		return $state;
 	}
 	
 	/*

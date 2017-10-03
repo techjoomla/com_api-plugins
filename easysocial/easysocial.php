c825fe3cb50484db5e8e65f53617cfeeb6e255e8
Pratiksha khandagale <pratiksha_k@techjoomla.com>
2017-10-03 16:25:28 +0530

    Task #111619 refactor: Making EasySocial api compatible with EasySocial(Stackidea) 2.1.0 Beta 1

--- easysocial/easysocial.php
+++ easysocial/easysocial.php
@@ -9,11 +9,13 @@ defined('_JEXEC') or die( 'Restricted access' );
 
 jimport('joomla.plugin.plugin');
 
+ES::import('site:/controllers/controller');
+
 /** plgAPIEasysocial
-* 
-* @since  1.8.8
-*/
-class plgAPIEasysocial extends ApiPlugin
+ *
+ * @since  1.8.8
+ */
+class PlgAPIEasysocial extends ApiPlugin
 {
 	/** Construct
 	 * 
@@ -22,18 +24,17 @@ class plgAPIEasysocial extends ApiPlugin
 	 */
 	public function __construct(&$subject, $config = array())
 	{
+		parent::__construct($subject = 'api', $config = array());
 
-		parent::__construct($subject = 'api' , $config = array());
+		ApiResource::addIncludePath(dirname(__FILE__) . '/easysocial');
 
-		ApiResource::addIncludePath(dirname(__FILE__).'/easysocial');
-		
-		/*load language file for plugin frontend*/ 
-		$lang = JFactory::getLanguage(); 
-		$lang->load('plg_api_easysocial', JPATH_ADMINISTRATOR,'',true);
-		$lang->load('com_easysocial', JPATH_ADMINISTRATOR,'',true);
-		$lang->load('com_easysocial', JPATH_SITE,'',true);
-        	$lang->load('com_users', JPATH_SITE,'',true);
-		$this->setResourceAccess('terms', 'public', 'post');	
-		$this->setResourceAccess('sociallogin', 'public', 'post');        			        		
+		/*load language file for plugin frontend*/
+		$lang = JFactory::getLanguage();
+		$lang->load('plg_api_easysocial', JPATH_ADMINISTRATOR, '', true);
+		$lang->load('com_easysocial', JPATH_ADMINISTRATOR, '', true);
+		$lang->load('com_easysocial', JPATH_SITE, '', true);
+		$lang->load('com_users', JPATH_SITE, '', true);
+		$this->setResourceAccess('terms', 'public', 'post');
+		$this->setResourceAccess('sociallogin', 'public', 'post');
 	}
 }

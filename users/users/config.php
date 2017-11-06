84b223b3b40116e27288bbbee156b6ef417ddd92
“Samadhan <samadhan_s@tekditechnologies.com>
2017-11-06 11:37:58 +0530

    New joomla user search API - Changed copyright info

--- users/users/config.php
+++ users/users/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package API plugins
- * @copyright Copyright (C) 2009 2017 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @copyright Copyright (C) 2009 - 2017 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
  * @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link http://www.techjoomla.com
 */

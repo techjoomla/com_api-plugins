cc015e2161a5ab1866acbe7f8c3153919883b106
Pratiksha khandagale <pratiksha_k@techjoomla.com>
2017-12-09 14:41:24 +0530

    Task #114679 refactor: [User Plugin] - Resolved Phpcs error and code updated

--- users/users/config.php
+++ users/users/config.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * @package API plugins
- * @copyright Copyright (C) 2009 2014 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
- * @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
- * @link http://www.techjoomla.com
-*/
-
+ * @package     Com_Api
+ * @subpackage  Com_api-plugins
+ *
+ * @copyright   Copyright (C) 2009-2017 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://techjoomla.com
+ * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
+ * and the com_api extension by Brian Edgerton (http://www.edgewebworks.com)
+ */
 defined('_JEXEC') or die( 'Restricted access' );
 
 jimport('joomla.plugin.plugin');
@@ -16,154 +19,264 @@ jimport('joomla.user.helper');
 jimport('joomla.user.user');
 jimport('joomla.application.component.helper');
 
-JModelLegacy::addIncludePath(JPATH_SITE.'components/com_api/models');
-require_once JPATH_SITE.'/components/com_api/libraries/authentication/user.php';
-require_once JPATH_SITE.'/components/com_api/libraries/authentication/login.php';
+JModelLegacy::addIncludePath(JPATH_SITE . 'components/com_api/models');
+require_once JPATH_SITE . '/components/com_api/libraries/authentication/user.php';
+require_once JPATH_SITE . '/components/com_api/libraries/authentication/login.php';
 
+/**
+ * API class UsersApiResourceConfig
+ *
+ * @since  1.0
+ */
 class UsersApiResourceConfig extends ApiResource
 {
+	/**
+	 * Method get
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.0
+	 */
 	public function get()
 	{
 		$obj = new stdClass;
-		//get joomla,easyblog and easysocial configuration
-		//get version of easysocial and easyblog
-		$easyblog = JPATH_ADMINISTRATOR .'/components/com_easyblog/easyblog.php';
-		$easysocial = JPATH_ADMINISTRATOR .'/components/com_easysocial/easysocial.php';
-		//eb version
-		if( JFile::exists( $easyblog ) )
+		$result = new stdClass;
+
+		$app = JFactory::getApplication();
+		$acl = $app->input->get('acl', false, boolean);
+		$userId = $app->input->get('userId', 0, 'INT');
+
+		if ($acl && $userId != 0)
 		{
-			$obj->easyblog = $this->getCompParams('com_easyblog','easyblog');
+			$this->getProfileACL($userId);
 		}
-		//es version
-		if( JFile::exists( $easysocial ) )
+		else
 		{
-			/*$xml = JFactory::getXML(JPATH_ADMINISTRATOR .'/components/com_easysocial/easyblog.xml');
-			$obj->easysocial_version = (string)$xml->version;*/
-			$obj->easysocial = $this->getCompParams( 'com_easysocial','easysocial' );
+		// Get joomla,easyblog and easysocial configuration
+		// Get version of easysocial and easyblog
+
+		$easyblog = JPATH_ADMINISTRATOR . '/components/com_easyblog/easyblog.php';
+		$easysocial = JPATH_ADMINISTRATOR . '/components/com_easysocial/easysocial.php';
+
+		// Eb version
+		if (JFile::exists($easyblog))
+		{
+			$obj->easyblog = $this->getCompParams('com_easyblog', 'easyblog');
+		}
+
+		// Es version
+		if (JFile::exists($easysocial))
+		{
+			$obj->easysocial = $this->getCompParams('com_easysocial', 'easysocial');
 		}
 
 		$obj->global_config = $this->getJoomlaConfig();
 		$obj->plugin_config = $this->getpluginConfig();
-			
 
-		$this->plugin->setResponse($obj);	
+		$installed_languages = JLanguageHelper::getLanguages();
+		$languages	=	array();
+
+		foreach ($installed_languages as $lang)
+		{
+			$languages[] = substr($lang->lang_code, 0, 2);
+		}
+
+		$obj->languages	=	$languages;
+
+		// $getACL = EB::acl($log_user);
+		// $obj->ACL = $getACL;
+
+		$xml = JFactory::getXML(JPATH_SITE . '/plugins/api/users/users.xml');
+		$obj->plugin_version = (string) $xml->version;
+
+		$result->result = $obj;
+
+		$this->plugin->setResponse($result);
+		}
 	}
 
+	/**
+	 * Method post
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.0
+	 */
 	public function post()
 	{
-	   $this->plugin->setResponse( JText::_( 'PLG_API_USERS_UNSUPPORTED_METHOD_POST' ));
+		$this->plugin->setResponse(JText::_('PLG_API_USERS_UNSUPPORTED_METHOD_POST'));
 	}
 
-	
-	//get component params
+	/**
+	 * Method to update auth keys
+	 *
+	 * @param   number  $cname  The  table
+	 * @param   number  $name   The table
+	 * 
+	 * @return  ApiPlugin response object
+	 *
+	 * @since 2.0
+	 */
 	public function getCompParams($cname=null,$name=null)
 	{
 		jimport('joomla.application.component.helper');
 		$app = JFactory::getApplication();
 		$cdata = array();
-	
-		$xml = JFactory::getXML(JPATH_ADMINISTRATOR .'/components/'.$cname.'/'.$name.'.xml');
-		$cdata['version'] = (string)$xml->version;
+
+		$xml = JFactory::getXML(JPATH_ADMINISTRATOR . '/components/' . $cname . '/' . $name . '.xml');
+		$cdata['version'] = (string) $xml->version;
 		$jconfig = JFactory::getConfig();
-		
-		if( $cname == 'com_easyblog' )
+
+		if ($cname == 'com_easyblog')
 		{
-		       /*$xml = JFactory::getXML(JPATH_ADMINISTRATOR .'/components/com_easyblog/easyblog.xml');
-                       $version = (string)$xml->version;*/  
+			if ($cdata['version'] < 5)
+			{
+				require_once JPATH_ROOT . '/components/com_easyblog/helpers/helper.php';
+				$eb_params = EasyBlogHelper::getConfig();
+			}
+			else
+			{
+				require_once JPATH_ADMINISTRATOR . '/components/com_easyblog/includes/easyblog.php';
+				$eb_params = EB::config();
+			}
 
-                       if($cdata['version']<5)
-                       {        
-                          require_once( JPATH_ROOT . '/components/com_easyblog/helpers/helper.php' );
-                               $eb_params        = EasyBlogHelper::getConfig();
-                       }
-                       else
-                       {        
-                          require_once JPATH_ADMINISTRATOR.'/components/com_easyblog/includes/easyblog.php';
-                               $eb_params = EB::config();
-                       }
-
-			$cdata['main_max_relatedpost'] = $eb_params->get('main_max_relatedpost');
-			$cdata['layout_pagination_bloggers'] = $eb_params->get('layout_pagination_bloggers');
-			$cdata['layout_pagination_categories'] = $eb_params->get('layout_pagination_categories');
-			$cdata['layout_pagination_categories_per_page'] = $eb_params->get('layout_pagination_categories_per_page');
-			$cdata['layout_pagination_bloggers_per_page'] = $eb_params->get('layout_pagination_bloggers_per_page');
-			$cdata['layout_pagination_archive'] = $eb_params->get('layout_pagination_archive');
-			$cdata['layout_pagination_teamblogs'] = $eb_params->get('layout_pagination_teamblogs');
-	
+			$cdata['main_max_relatedpost']					= $eb_params->get('main_max_relatedpost');
+			$cdata['layout_pagination_bloggers']			= $eb_params->get('layout_pagination_bloggers');
+			$cdata['layout_pagination_categories']			= $eb_params->get('layout_pagination_categories');
+			$cdata['layout_pagination_categories_per_page']	= $eb_params->get('layout_pagination_categories_per_page');
+			$cdata['layout_pagination_bloggers_per_page']	= $eb_params->get('layout_pagination_bloggers_per_page');
+			$cdata['layout_pagination_archive']				= $eb_params->get('layout_pagination_archive');
+			$cdata['layout_pagination_teamblogs']			= $eb_params->get('layout_pagination_teamblogs');
 		}
 		else
 		{
-			require_once JPATH_ADMINISTRATOR.'/components/com_easysocial/includes/foundry.php';
+			require_once JPATH_ADMINISTRATOR . '/components/com_easysocial/includes/foundry.php';
 			$es_params = FD::config();
-			$profiles = FD::model( 'profiles' );
+			$profiles = FD::model('profiles');
 
-			//$cdata['conversations_limit'] = $es_params->get('conversations')->limit;
-			$cdata['activity_limit'] = $es_params->get('activity')->pagination;
-			$cdata['lists_limit'] = $es_params->get('lists')->display->limit;
-			$cdata['comments_limit'] = $es_params->get('comments')->limit;
-			$cdata['stream_pagination_limit'] = $es_params->get('stream')->pagination->pagelimit;
-			$cdata['photos_pagination_limit'] = $es_params->get('photos')->pagination->photo;
-			$cdata['album_pagination_limit'] = $es_params->get('photos')->pagination->album;
-			$cdata['emailasusername'] = $es_params->get('registrations')->emailasusername;
-			$cdata['displayName'] = $es_params->get('users')->displayName;
-			$cdata['groups']['enabled'] = $es_params->get('groups')->enabled;
-			$profiles_data = $profiles->getAllProfiles();
+			// $cdata['conversations_limit'] = $es_params->get('conversations')->limit;
+			$cdata['activity_limit']			= $es_params->get('activity')->pagination;
+			$cdata['lists_limit']				= $es_params->get('lists')->display->limit;
+			$cdata['comments_limit']			= $es_params->get('comments')->limit;
+			$cdata['stream_pagination_limit']	= $es_params->get('stream')->pagination->pagelimit;
+			$cdata['photos_pagination_limit']	= $es_params->get('photos')->pagination->photo;
+			$cdata['album_pagination_limit']	= $es_params->get('photos')->pagination->album;
+			$cdata['emailasusername']			= $es_params->get('registrations')->emailasusername;
+			$cdata['displayName']				= $es_params->get('users')->displayName;
+			$cdata['groups']['enabled']			= $es_params->get('groups')->enabled;
+			$profiles_data						= $profiles->getAllProfiles();
 
 			/* Check for profile_type is allowed for Registration by vivek*/
 			$allowed_profile_types = array();
-			foreach ($profiles_data as $key ) {
-				if($key->registration == '1'){
+
+			foreach ($profiles_data as $key)
+			{
+				if ($key->registration == '1' && $key->state == '1')
+				{
 					array_push($allowed_profile_types, $key);
 				}
 			}
+
 			$cdata['profile_types'] = $allowed_profile_types;
 		}
+
 		return $cdata;
 	}
-	
-		// get fb plugin config
+
+	/**
+	 * Method get fb plugin config
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.0
+	 */
 	public function getpluginConfig()
 	{
 		$data = array();
 		$plugin = JPluginHelper::getPlugin('api', 'users');
 		$pluginParams = new JRegistry($plugin->params);
-		//code for future use
-		/*$plugin_es = JPluginHelper::getPlugin('api', 'easysocial');
-		$pluginParams_es = new JRegistry($plugin_es->params);*/
-		
+
+		// Code for future use
+
+		/* $plugin_es = JPluginHelper::getPlugin('api', 'easysocial');
+		$pluginParams_es = new JRegistry($plugin_es->params); */
+
 		$data['fb_login'] = $pluginParams->get('fb_login');
 		$data['fb_app_id'] = $pluginParams->get('fb_app_id');
-		$data['quick2art'] = $pluginParams->get('quick2art');
-		
+		$data['google_client_id']	=	$pluginParams->get('google_client_id');
+
 		return $data;
 	}
 
-	
-	//get joomla config changes
+	/**
+	 * Method get joomla config changes
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.0
+	 */
 	public function getJoomlaConfig()
 	{
-		$jconfig = JFactory::getConfig();
-		$jarray = array();
-		$jarray['global_list_limit'] = $jconfig->get('list_limit');
-		$jarray['offset'] = $jconfig->get('offset');
-		$jarray['offset_user'] = $jconfig->get('offset_user');
-		
+		$jconfig						= JFactory::getConfig();
+		$jarray 						= array();
+		$jarray['global_list_limit']	= $jconfig->get('list_limit');
+		$jarray['offset']				= $jconfig->get('offset');
+		$jarray['offset_user']			= $jconfig->get('offset_user');
+
 		return $jarray;
-	}	
-	
-	/*
-	 * function to update Easyblog auth keys
+	}
+
+	/**
+	 * Method to update Easyblog auth keys
+	 *
+	 * @param   string  $user  The  table
+	 * @param   string  $key   The table
+	 * 
+	 * @return  ApiPlugin response object
+	 *
+	 * @since 2.0
 	 */
 	public function updateEauth($user=null,$key=null)
 	{
-		require_once JPATH_ADMINISTRATOR.'/components/com_easysocial/includes/foundry.php';
-		$model 	= FD::model('Users');
-		$id 	= $model->getUserId('username', $user->username);
-		$user 	= FD::user($id);
-		$user->alias = $user->username;
-		$user->auth = $key;
+		require_once JPATH_ADMINISTRATOR . '/components/com_easysocial/includes/foundry.php';
+		$model			= FD::model('Users');
+		$id 			= $model->getUserId('username', $user->username);
+		$user			= FD::user($id);
+		$user->alias	= $user->username;
+		$user->auth 	= $key;
 		$user->store();
-	
+
 		return $id;
+	}
+
+	/**
+	 * Method use to get ACL config
+	 *
+	 * @param   string  $userId  The  table
+	 * 
+	 * @return  ApiPlugin response object
+	 * 	
+	 * @since  1.0
+	 */
+	public function getProfileACL($userId)
+	{
+		$access = ES::access($userId, SOCIAL_TYPE_USER);
+		$res = new stdClass;
+
+		$res->result->comments		= $access->get('comments');
+		$res->result->conversations	= $access->get('conversations');
+		$res->result->events		= $access->get('events');
+		$res->result->files			= $access->get('files');
+		$res->result->friends		= $access->get('friends');
+		$res->result->groups		= $access->get('groups');
+		$res->result->albums		= $access->get('albums');
+		$res->result->photos		= $access->get('photos');
+		$res->result->polls			= $access->get('polls');
+		$res->result->reports		= $access->get('reports');
+		$res->result->story			= $access->get('story');
+		$res->result->stream		= $access->get('stream');
+		$res->result->videos		= $access->get('videos');
+
+		$this->plugin->setResponse($res);
 	}
 }
--- users/users/login.php
+++ users/users/login.php
@@ -1,124 +1,154 @@
 <?php
 /**
- * @package API plugins
- * @copyright Copyright (C) 2009 2014 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
- * @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
- * @link http://www.techjoomla.com
+ * @package     Com_Api
+ * @subpackage  Com_api-plugins
+ *
+ * @copyright   Copyright (C) 2009-2017 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://techjoomla.com
+ * Work derived from the original RESTful API by Techjoomla (https://github.com/techjoomla/Joomla-REST-API)
+ * and the com_api extension by Brian Edgerton (http://www.edgewebworks.com)
  */
 
 defined('_JEXEC') or die( 'Restricted access' );
-
-jimport('joomla.plugin.plugin');
-jimport('joomla.html.html');
-jimport('joomla.application.component.controller');
-jimport('joomla.application.component.model');
-jimport('joomla.user.helper');
-jimport('joomla.user.user');
-jimport('joomla.application.component.helper');
-
 JModelLegacy::addIncludePath(JPATH_SITE . 'components/com_api/models');
 require_once JPATH_SITE . '/components/com_api/libraries/authentication/user.php';
 require_once JPATH_SITE . '/components/com_api/libraries/authentication/login.php';
 require_once JPATH_SITE . '/components/com_api/models/key.php';
 require_once JPATH_SITE . '/components/com_api/models/keys.php';
 
+/**
+ * API class UsersApiResourceConfig
+ *
+ * @since  1.0
+ */
 class UsersApiResourceLogin extends ApiResource
 {
+	/**
+	 * Method get
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.0
+	 */
 	public function get()
 	{
-		$this->plugin->setResponse( JText::_('PLG_API_USERS_GET_METHOD_NOT_ALLOWED_MESSAGE'));
+		$this->plugin->err_code		= 405;
+		$this->plugin->err_message	= JText::_('PLG_API_USERS_GET_METHOD_NOT_ALLOWED_MESSAGE');
+		$this->plugin->setResponse(null);
 	}
 
+	/**
+	 * Method post
+	 *
+	 * @return  auth, id
+	 *
+	 * @since 1.0
+	 */
 	public function post()
 	{
 		$this->plugin->setResponse($this->keygen());
 	}
 
+	/**
+	 * Method description
+	 *
+	 * @return  key
+	 *
+	 * @since 1.0
+	 */
 	public function keygen()
 	{
-		//init variable
-		$obj = new stdclass;
-		$umodel = new JUser;
-		$user = $umodel->getInstance();       
+		// Init variable
+		$obj		= new stdclass;
+		$umodel		= new JUser;
+		$user		= $umodel->getInstance();
+		$app		= JFactory::getApplication();
+		$username	= $app->input->get('username', 0, 'STRING');
+		$user		= JFactory::getUser();
+		$id			= JUserHelper::getUserId($username);
 
-		$app = JFactory::getApplication();
-		$username = $app->input->get('username', 0, 'STRING');
-
-		$user = JFactory::getUser();
-		$id = JUserHelper::getUserId($username);
-
-		if($id == null)
+		if ($id == null)
 		{
 			$model = FD::model('Users');
-			$id = $model->getUserId('email', $username);            
+			$id = $model->getUserId('email', $username);
 		}
 
-		$kmodel = new ApiModelKey;
-		$model = new ApiModelKeys;
-		$key = null;
+		$result	=	new stdClass;
+		$kmodel	=	new ApiModelKey;
+		$model	=	new ApiModelKeys;
+		$key	=	null;
+
 		// Get login user hash
-		//$kmodel->setState('user_id', $user->id);
+		// $kmodel->setState('user_id', $user->id);
 		$kmodel->setState('user_id', $id);
 		$log_hash = $kmodel->getList();
 		$log_hash = (!empty($log_hash))?$log_hash[count($log_hash) - count($log_hash)]:$log_hash;
 
-		if( !empty($log_hash) )
+		if (!empty($log_hash))
 		{
 			$key = $log_hash->hash;
 		}
-		elseif( $key == null || empty($key) )
+		elseif($key == null || empty($key))
 		{
-				// Create new key for user
-				$data = array(
-				'userid' => $user->id,
-				'domain' => '' ,
-				'state' => 1,
-				'id' => '',
-				'task' => 'save',
-				'c' => 'key',
-				'ret' => 'index.php?option=com_api&view=keys',
-				'option' => 'com_api',
-				JSession::getFormToken() => 1
-				);
+			// Create new key for user
+			$data = array(
+			'userid'	=> $user->id,
+			'domain'	=> '' ,
+			'state'		=> 1,
+			'id'		=> '',
+			'task'		=> 'save',
+			'c'			=> 'key',
+			'ret'		=> 'index.php?option=com_api&view=keys',
+			'option'	=> 'com_api',
+			JSession::getFormToken() => 1
+			);
 
-				$result = $kmodel->save($data);
-				$key = $result->hash;
-				
-				//add new key in easysocial table
-				$easyblog = JPATH_ROOT . '/administrator/components/com_easyblog/easyblog.php';
-				if (JFile::exists($easyblog) && JComponentHelper::isEnabled('com_easysocial', true))
-				{
-					$this->updateEauth( $user , $key );
-				}
+			$result = $kmodel->save($data);
+			$key = $result->hash;
+
+			// Add new key in easysocial table
+			$easyblog = JPATH_ROOT . '/administrator/components/com_easyblog/easyblog.php';
+
+			if (JFile::exists($easyblog) && JComponentHelper::isEnabled('com_easysocial', true))
+			{
+				$this->updateEauth($user, $key);
+			}
 		}
-		
-		if( !empty($key) )
+
+		if (!empty($key))
 		{
-			$obj->auth = $key;
-			$obj->code = '200';
-			//$obj->id = $user->id;
-			$obj->id = $id;
+			$result->result 		=	new stdClass;
+			$result->result->token	=	$key;
+			$result->result->id		=	$id;
 		}
 		else
 		{
-			$obj->code = 403;
-			$obj->message = JText::_('PLG_API_USERS_BAD_REQUEST_MESSAGE');
+			$this->plugin->err_code = 403;
+			$this->plugin->err_message = JText::_('PLG_API_USERS_BAD_REQUEST_MESSAGE');
 		}
-		return( $obj );
-	
+
+		return $result;
 	}
-	
-	/*
-	 * function to update Easyblog auth keys
+
+	/**
+	 * Method updateEauth
+	 *
+	 * @param   array  $user  my 
+	 * @param   array  $key   group object
+	 * 
+	 * @return string
+	 *
+	 * @since 1.0
 	 */
 	public function updateEauth($user=null,$key=null)
 	{
-		require_once JPATH_ADMINISTRATOR.'/components/com_easysocial/includes/foundry.php';
-		$model 	= FD::model('Users');
-		$id 	= $model->getUserId('username', $user->username);
-		$user 	= FD::user($id);
-		$user->alias = $user->username;
-		$user->auth = $key;
+		require_once JPATH_ADMINISTRATOR . '/components/com_easysocial/includes/foundry.php';
+		$model			= FD::model('Users');
+		$id 			= $model->getUserId('username', $user->username);
+		$user			= FD::user($id);
+		$user->alias	= $user->username;
+		$user->auth		= $key;
 		$user->store();
 
 		return $id;
